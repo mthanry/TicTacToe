e2075b4a3fef9eac7783be1056630c88cdb4c515
Michael Thanry <mthanry@gmail.com>
2020-12-12 20:53:01 +0000

    Assignment commit

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,18 +1,51 @@
-from os import system, name 
+from os import system, name
 
+def minimax(node, depth, maxPlayer, maxTurn):
+    if node.is_winner(maxPlayer):
+        return 1 #Maximized player wins
+    elif node.is_winner(maxPlayer.next()):
+        return -1 #Minimized player wins
+    # There is no winner and explored as far as desired, it's a draw
+    elif depth == 0 :
+        return 0
+
+    scores = []
+
+    # Pretend to play (recusively)
+    for cell in node.freecells():
+        # Who plays?
+        if maxTurn:
+            node.play(maxPlayer,cell)
+        else:
+            node.play(maxPlayer.next(),cell)
+
+        #Keep track of the score for that move
+        scores.append(minimax(node, depth - 1, maxPlayer, not maxTurn))
+
+        # Rollback
+        node.cancel()
+
+    if maxTurn:
+        return max(scores)
+    else:
+        return min(scores) 
+        
 class Player():
+    instances = []
     def __init__(self, name, mark, ai = False):
         self.name = name
         self.mark = mark
         self.ai = ai
-        self.score = 0
+        self.instances.append(self)
+        self.id = len(self.instances)-1
 
-    def wins(self):
-        self.score =+ 1
+    def next(self): # Returns the next player object
+        next = self.instances[(self.id + 1) % len(self.instances)]
+        return next
 
 class TicTacToe():
-    def __init__(self):
-        self.board = [None]*9
+    def __init__(self, board = [None]*9):
+        self.board = board
         self.history = []
         self.outcome = None
         self.over = False
@@ -21,6 +54,7 @@ class TicTacToe():
             [0,3,6],[1,4,7],[2,5,8],    #cols
             [0,4,8],[6,4,2]             #diagonals
         ]
+        self.children = []
 
     def display(self):
         """
@@ -53,12 +87,13 @@ class TicTacToe():
         # Has the player ticked all cells of any winning combination?
         for combo in self.COMBOS:
             if all(list(map(lambda cell: self.board[cell] == player.mark, combo))):
-                return True
-        return False
+                return player.name
+        return None
 
     def play(self, player, cell):
+
         self.board[cell] = player.mark
-        self.history.append((cell,player))
+        self.history.append(cell)
         
         # Finish the game if no move can be made
         if len(self.freecells()) == 0:
@@ -68,6 +103,10 @@ class TicTacToe():
         if self.is_winner(player):
             self.outcome = player.name
             self.over = True
+
+    def cancel(self):
+        cell = self.history.pop()
+        self.board[cell] = None
 
 def clear(): 
     """Clear the terminal"""
@@ -83,9 +122,8 @@ def getcellfromplayer(game, player):
     """
     Asks the user to make a move
         Parameters:
-            board (list):   A list of cells that represents 
-                            the TicTacToe board
-            player (str):   A player object 
+            game:   An instance of the TicTacToe object
+            player:   A player object 
 
         Returns:
             cell (integer): A valid cell number where the 
@@ -101,121 +139,7 @@ def getcellfromplayer(game, player):
                 cell = int(cell)
                 valid = True
     return cell-1
-
-def getcellfromAI(game, player,opponent):
-    """
-    Asks the user to make a move
-        Parameters:
-            board (list):   A list of cells that represents 
-                            the TicTacToe board
-            player (obj):   A player object
-            opponent (obj): A player object
-
-        Returns:
-            cell (integer): A valid cell number where the 
-                            player can put his mark
-    """
-    
-    # HillClimb algorithm
-    
-    scores = []
-    strat_1 = []
-    strat_2 = []
-    strat_3 = []
-    strat_41 = []
-    strat_42 = []
-    strat_5 = []
-    strat_6 = []
-    strat_7 = []
-    strat_8 = []
-
-    # Evaluate each possible next move against the optimal strategy
-    for move in game.freecells():   
-        me = 0
-        him = 0
-        combo_stats = []
-
-        # For line, row, diag that contains the evaluated move
-        for combo in game.COMBOS:
-            if move in combo:
-                # assess the the number of marks each player has 
-                me = len(list(filter(lambda x: game.board[x] == player.mark, combo)))
-                him = len(list(filter(lambda x: game.board[x] == opponent.mark, combo)))
-                combo_stats.append((me,him))
-
-        
-        # Heuristics calculation below are based on the best strategy
-        # See https://en.wikipedia.org/wiki/Tic-tac-toe#Strategy
-
-        # 1. WIN
-        # If combo consists of 2 of the player's mark then fill the space
-        if (2,0) in combo_stats: 
-            strat_1.append(move)
-            continue
-
-        # 2. BLOCK
-        # If combo consists of 2 of the opponent's mark then fill the space
-        if (0,2) in combo_stats: 
-            strat_2.append(move)
-            continue         
-
-        # 3. FORK
-        # Create a double opportunity
-        if combo_stats.count((1,0)) == 2: 
-            strat_3.append(move)
-            continue
-
-        # 4. BLOCK FORK
-        # Prevent a double opportunity
-        if combo_stats.count((0,1)) == 2: 
-            strat_42.append(move)
-            if len(strat_42) >1:
-                # find a valid move in a combo with (me/him) = (1,0) 
-                # that contains none of the values of strat_42[]
-                for combo in game.COMBOS:
-                    if (    strat_42[0] not in combo and 
-                            strat_42[1] not in combo and
-                            len(list(filter(lambda x: game.board[x] == player.mark, combo))) == 1 and
-                            len(list(filter(lambda x: game.board[x] == opponent.mark, combo))) == 0):
-                        strat_41.extend(list(filter(lambda x: game.board[x] == None, combo)))
-                
-            continue
-
-        # 5. CENTRE
-        if game.board[4] == None: 
-            strat_5.append(4)
-            continue
-
-        # 6. OPPOSITE CORNER
-        if  ((move == 0 and game.board[9] == opponent.mark) or 
-            ( move == 9 and game.board[0] == opponent.mark) or 
-            (move == 3 and game.board[7] == opponent.mark) or 
-            (move == 7 and game.board[3] == opponent.mark)): 
-            strat_6.append(move)
-            continue
-        
-        # 7. EMPTY CORNER
-        if move in (0, 3, 7, 9): 
-            strat_7.append(move)
-            continue
-            
-        # 8. EMPTY EDGE
-        if move in (1, 4, 6, 8): 
-            strat_8.append(move)
-            continue
-
-    scores.extend(strat_1)
-    scores.extend(strat_2)
-    scores.extend(strat_3)
-    scores.extend(strat_41)
-    scores.extend(strat_42)
-    scores.extend(strat_5)
-    scores.extend(strat_6)
-    scores.extend(strat_7)
-    scores.extend(strat_8)
-
-    return scores[0]
-            
+     
 # Prematch setup
 player = [None]*2
 player[0] = Player("Michael","X", False)
@@ -223,23 +147,39 @@ player[1] = Player("WOPR","O", True) # Easter egg https://www.youtube.com/watch?
 who = 0             # Which player starts?
 game = TicTacToe()
 
+
 # MATCH LOOP
 while not game.over:
-    game.display()
-    print()
 
+    # clear the screen
+    clear()
+
+    # display the board
+    game.display()
+    
     # Prompt the player to play
     if player[who].ai:
-        cell = getcellfromAI(game, player[who], player[(who + 1) % len(player)])
+        scores = []
+        for c in game.freecells():
+            #create a new to break the reference to game.board
+            node = TicTacToe(game.board)
+
+            #pretend to play a cell to get a score
+            node.play(player[who],c)
+            scores.append(minimax(node, len(game.freecells()), player[who],False))
+            node.cancel()
+        
+        #get the cell with the best score
+        cell = game.freecells()[scores.index(max(scores))]
+
     else:
         cell = getcellfromplayer(game, player[who])
 
-    # Play in that cell
+    # Play that cell
     game.play(player[who], cell)
     
-    # Move to the next player and clear the screen
+    # Move to the next player
     who = (who + 1) % len(player)
-    clear()
     
 # Match is over, display the final board and the outcome
 game.display()
